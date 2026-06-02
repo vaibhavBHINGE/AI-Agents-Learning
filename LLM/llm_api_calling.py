765e110ef87165652d057f8bde38a3ad684918dd
Vaibhav Bhinge <vbhinge2711@gmail.com>
2026-06-02 23:14:16 +0530

    changes model input to model

--- LLM/llm_api_calling.py
+++ LLM/llm_api_calling.py
@@ -5,7 +5,7 @@ from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint  # huggin
 from dotenv import load_dotenv  # to call an LLM model we need to have api key that is stored in .env file
 load_dotenv()
 
-llm = HuggingFaceEndpoint(repo_id="deepseek-ai/DeepSeek-V4-Pro") # calling model
+llm = HuggingFaceEndpoint(model="deepseek-ai/DeepSeek-V4-Pro") # calling model
 chat_model=ChatHuggingFace(llm=llm)
 
 output=chat_model.invoke("how many planets are in our solar system?")  # invoking/ passing message prompt to LLM model
--- LLM/llm_settings.py
+++ LLM/llm_settings.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 llm_obj=HuggingFaceEndpoint(
-    repo_id="deepseek-ai/DeepSeek-V4-Pro",    # to extract the model from HuggingFace we need api token that is stored in .env file we importing that using load_dotenv() function
+    model="deepseek-ai/DeepSeek-V4-Pro",    # to extract the model from HuggingFace we need api token that is stored in .env file we importing that using load_dotenv() function
     temperature=0.7, # controls the randomness of the model's responses. A higher temperature (e.g., 1.0) will make the model's output more diverse and creative, while a lower temperature (e.g., 0.2) will make it more focused and deterministic.
     max_new_tokens=400, # the maximum number of tokens to generate in the completion.
     top_p=0.9, # controls the diversity of the model's output by limiting the token selection to a subset of the most probable tokens. A higher top_p (e.g., 0.9) will allow for more diverse responses, while a lower top_p (e.g., 0.5) will make the output more focused and deterministic.
