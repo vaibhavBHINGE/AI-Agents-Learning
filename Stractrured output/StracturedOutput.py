24ce709c9e99e5efa93002b06be3a8a26b2f4902
Vaibhav Bhinge <vbhinge2711@gmail.com>
2026-06-02 23:18:58 +0530

    update schema code block to pydantic

--- Stractrured output/StracturedOutput.py
+++ Stractrured output/StracturedOutput.py
@@ -1,34 +1,28 @@
 from dotenv import load_dotenv
 load_dotenv(dotenv_path=r"F:\Gen AI\API calling\Ollama\.env")
 from langchain_core.messages import HumanMessage, SystemMessage
-from langchain_google_genai import ChatGoogleGenerativeAI
-from typing import TypedDict,Annotated
-
-model = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash"
-    # temperature=1.0,  # Gemini 3.0+ defaults to 1.0
-    # max_tokens=None,
-    # timeout=None,
-    # max_retries=2,
-    # # other params...
-)
+from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
+from pydantic import BaseModel, Field
+llm_obj=HuggingFaceEndpoint(model="deepseek-ai/DeepSeek-V4-Pro")
+chat_model=ChatHuggingFace(llm=llm_obj)
 
 
-# Schema for typeDict 
-class Response(TypedDict):
+# Schema for Pydantic 
+class Response(BaseModel):
+    sentiment: str = Field(description="The sentiment of the review")
+    tone: str = Field(description="The tone of the review")
+    summary: str = Field(description="A summary of the review")
+    your_rating: int = Field(description="Your rating of the product")
 
-    sentiment: Annotated[str, "The sentiment of the review"]
-    tone: Annotated[str, "The tone of the review"]
-    summery: Annotated[str, "A summary of the review"]
-    your_rating: Annotated[int, "Your rating of the product"]
-
-model_Response=model.with_structured_output(Response)
+model_Response=chat_model.with_structured_output(Response) 
 print("model_response: ",model_Response)
-output=model_Response.invoke("""Not much comfortable for those who have wider finger area, after using this for months I am writing this review.
-An yellow stain are appearing on the clothes which make this item less appealing so if you are able to manage it you can purchase this. Overall this item givs nice looks and will be comfortable for those who are having lean foot structure.
+
+query="""Not much comfortable for those who have wider finger area, after using this for months I am writing this review.
+An yellow stain are appearing on the clothes which make this item less appealing so if you are able to manage it you can purchase this. Overall this item gives nice looks and will be comfortable for those who are having lean foot structure.
 Not much heavy
 Nice grip
-Can pair with casuals.""")
+Can pair with casuals."""
+output=model_Response.invoke(query)
 
 print(output)
 
