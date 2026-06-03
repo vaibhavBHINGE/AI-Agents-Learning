9e7a7a8f587b3d159ffb994f6c608882ee564d27
Vaibhav Bhinge <vbhinge2711@gmail.com>
2026-06-03 23:14:11 +0530

    simple chain example

---
+++ Chain/SimpleChian.py
@@ -0,0 +1,24 @@
+from langchain_huggingface import  HuggingFaceEndpoint,ChatHuggingFace
+from dotenv import load_dotenv
+load_dotenv()
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+llm_obj = HuggingFaceEndpoint(model="deepseek-ai/DeepSeek-V4-Flash")
+
+model = ChatHuggingFace(llm=llm_obj)
+
+parser = StrOutputParser()
+
+prompt = PromptTemplate(
+    template="list top 5 interesting facts about {topic}.",
+    input_variables=["topic"]
+)
+
+chain=prompt | model | parser
+print(chain)
+print(prompt)
+result=chain.invoke({"topic": "cricket"})
+print(result)
+
+chain.get_graph().print_ascii()  # To visualize the chain structure
