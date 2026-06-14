16662fe0013e00d3f7a8382f59e65b039dfa32ae
Vaibhav Bhinge <vbhinge2711@gmail.com>
2026-06-14 23:20:09 +0530

    documnet starcture based retriving

---
+++ Rag/TextSpliter/DocumentStractureBased.py
@@ -0,0 +1,32 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+text=r"""from langchain_huggingface import HuggingFaceEndpoint,ChatHuggingFace
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser,PydanticOutputParser
+from langchain_core.runnables import  RunnableBranch,RunnableLambda
+from langchain_community.document_loaders import TextLoader
+from dotenv import load_dotenv
+load_dotenv()
+
+modelHuggingFace=ChatHuggingFace(llm=HuggingFaceEndpoint( repo_id="deepseek-ai/DeepSeek-V4-Flash"))
+
+strParser = StrOutputParser()
+
+document=TextLoader(r"C:\Users\vbhin\Documents\workspace-spring-tool-suite-4-4.21.0.RELEASE\.metadata\.plugins\org.eclipse.jdt.core\javaLikeNames.txt").load()
+
+prompt=PromptTemplate(
+    
+    template="What is in: {document}",
+    input_variables=["document"],
+)
+
+chain=prompt | modelHuggingFace | strParser
+
+output=chain.invoke({"document": document[0].page_content})
+print(output)"""
+
+splitter = RecursiveCharacterTextSplitter.from_language(language="python", chunk_size=100, chunk_overlap=0)
+
+result = splitter.split_text(text)
+print(len(result))
+print(result[1])git
